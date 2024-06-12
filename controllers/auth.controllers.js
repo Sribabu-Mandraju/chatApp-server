b3b5e13e7054226f28eaa588d71a2633d452c611
sribabu <sribabumandraju@gmail.com>
2024-06-12 21:54:20 +0530

    saving

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -64,66 +64,9 @@ export const getUserById = async (req,res,next) => {
 }
 
 
+
+
 export const signUp = async (req, res, next) => {
-    const {name,password,email,ID} = req.body
-
-    const hashed = await hashPassword(password)
-    
-
-    try {
-        let oldUser = await User.findOne({email:email});
-        if (oldUser) {
-            return res.status(400).json({
-                "message": "User already exists",
-            });
-        }
-
-       else{
-        const user = new User({
-            name,
-            "password":hashed,
-            email,
-            ID,
-        });
-        await user.save();
-
-        return res.status(201).json({ user });
-       }
-    } catch (err) {
-        console.error("Error while signing up:", err);
-        return res.status(500).json({
-            "message": "Internal server error"
-        });
-    }
-};
-
-
-export const signin = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Invalid password" });
-        }
-
-        return res.status(200).json({ user });
-    } catch (error) {
-        console.error("Error while signing in:", error);
-        return res.status(500).json({ message: "Internal server error" });
-    }
-};
-
-
-
-export const signUp2 = async (req, res, next) => {
     const { name, email, password ,ID} = req.body;
 
     try {
@@ -186,6 +129,9 @@ export const signIn = async (req, res, next) => {
 
         // Passwords match, generate JWT token and return it
         const token = generateToken(user);
+        res.cookie("jwt",token,{
+            httpOnly:true
+        })
 
         return res.status(200).json({ token });
     } catch (err) {
@@ -196,51 +142,17 @@ export const signIn = async (req, res, next) => {
     }
 };
 
+export const logout =async  (req,res) => {
+ try {
+    res.cookie("jwt","")
+    res.status(200).json({
+        message:"logged out successfully"
+    })
+ } catch (error) {
+    console.log("error while logging out ",error.message)
 
-// const generateToken2 = (user) => {
-//     return jwt.sign(
-//         {
-//             _id: user._id,
-//             email: user.email,
-//             name: user.name,
-//         },
-//         process.env.JWT_SECRET, // Private key for signing JWT
-//         {
-//             expiresIn: '1h', // Token expires in 1 hour (adjust as needed)
-//         }
-//     );
-// };
-
-// export const login = async (req, res, next) => {
-//     const { email, password } = req.body;
-
-//     try {
-//         // Check if the user exists
-//         let user = await User.findOne({ email });
-
-//         if (!user) {
-//             return res.status(404).json({
-//                 message: "User not found",
-//             });
-//         }
-
-//         // Compare the entered password with the hashed password
-//         const isMatch = await bcrypt.compare(password, user.password);
-
-//         if (!isMatch) {
-//             return res.status(400).json({
-//                 message: "Invalid credentials",
-//             });
-//         }
-
-//         // Passwords match, generate JWT token and return it
-//         const token = generateToken(user);
-
-//         return res.status(200).json({ token });
-//     } catch (err) {
-//         console.error("Error while logging in:", err);
-//         return res.status(500).json({
-//             message: "Internal server error",
-//         });
-//     }
-// };
+    res.status(500).json({
+        message:error.message
+    })
+ }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,8 @@
 import express from 'express'
 const router = express.Router()
-import { signUp ,signIn,signUp2} from '../controllers/auth.controllers.js'
+import { signIn,signUp} from '../controllers/auth.controllers.js'
 
-router.post("/signup",signUp)
 router.post("/signin",signIn)
-router.post("/signup2",signUp2)
+router.post("/signup",signUp)
 
 export default router
